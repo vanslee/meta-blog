7b3ec4b6abd1062f4e415ede7dcaba4a50caf5b2
lidengxiang <vanslee62@gmail.com>
2023-06-03 11:51:24 +0800

    删除跨域

--- ldx_blog_java/src/main/java/com/ldx/blog/config/AuthConfig.java
+++ ldx_blog_java/src/main/java/com/ldx/blog/config/AuthConfig.java
@@ -1,11 +1,7 @@
 package com.ldx.blog.config;
 
 import cn.dev33.satoken.stp.StpUtil;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,19 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AuthConfig implements WebMvcConfigurer {
     private static final long MAX_AGE = 24 * 60 * 60;
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("https://www.lidengxiang.top:8000");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.setMaxAge(MAX_AGE);
-        source.registerCorsConfiguration("/**",corsConfiguration);
-        return new CorsFilter(source);
-    }
-
+    
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 

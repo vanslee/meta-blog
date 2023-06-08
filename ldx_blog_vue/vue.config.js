bfe795422444150907901fd83b5e13b242bc9cef
lidengxiang <vanslee62@gmail.com>
2023-06-08 20:42:40 +0800

    修改.env文件

--- ldx_blog_vue/vue.config.js
+++ ldx_blog_vue/vue.config.js
@@ -1,15 +1,26 @@
 'use strict'
 // const { defineConfig } = require('@vue/cli-service')
+console.log(`enviroment: ${process.env.NODE_ENV}`)
 module.exports = {
   lintOnSave: false,
-  // publicPath: process.env.NODE_ENV === 'production'? ''
   devServer: {
+    onListening: ({ server }) => {
+      const { host, port } = server
+      console.log(`${host}:${port}`)
+      console.log(`enviroment: ${process.env.NODE_ENV}`)
+    },
     proxy: {
       '/apis': {
         ws: true,
         changeOrigin: true,
-        target: 'http://localhost:8000/'
+        target: process.env.NODE_ENV === 'development' ? 'http://localhost:8000/' : 'https://www.lidengxiang.top/'
       }
+    }
+  },
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: '李图报日记'
     }
   },
   transpileDependencies: true
